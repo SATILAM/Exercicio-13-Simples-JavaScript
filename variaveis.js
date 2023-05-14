3555ecc8789098fe71094bf043874e6cf3564f8f
Átila Soares Mendonça <atilasoaresmendonca@gmail.com>
2023-05-14 13:46:24 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,18 @@
+let num1 = 10;
+let num2 = 5;
+
+// Soma
+let soma = num1 + num2;
+console.log("A soma é:", soma);
+
+// Subtração
+let subtracao = num1 - num2;
+console.log("A subtração é:", subtracao);
+
+// Divisão
+let divisao = num1 / num2;
+console.log("A divisão é:", divisao);
+
+// Multiplicação
+let multiplicacao = num1 * num2;
+console.log("A multiplicação é:", multiplicacao);
